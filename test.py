2757c27a6019efbfe22117819b62fe7c01105b30
Martijn Willems <martijn.willems@altran.com>
2020-10-21 16:51:48 +0200

    maybe this works

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import os
 #mlflow.set_tracking_uri(remote_server_uri)
 # Note: on Databricks, the experiment name passed to mlflow_set_experiment must be a
 # valid path in the workspace
-mlflow.set_experiment("/mlflowtest")
+# mlflow.set_experiment("/mlflowtest")
 with mlflow.start_run():
     mlflow.log_param("a", 1)
     mlflow.log_metric("b", 2)
